e1f719ad6472fa43b001203c42f8f9b593196e74
Sergey <110375004+SergeyMark@users.noreply.github.com>
2022-12-23 16:18:18 +0200

    add up

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+function createGallery(){
+    return galleryItems.map( (itemGallery) => 
+        `<a class="gallery__item" href="${itemGallery.original}">
+            <img class="gallery__image" src="${itemGallery.preview}" alt="${itemGallery.description}" />
+        </a>`
+    ).join("")};
+
+const createGalleryFn = createGallery();
+
+gallery.insertAdjacentHTML('beforeend', createGalleryFn);
+
+new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,37 @@
+import Player from '@vimeo/player';
 
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+// const KEY_LOCAL_STORAGE = "videoplayer-current-time";
+
+// player.on('timeupdate', function() {
+//     const videoplayerCurrentTime = "videoplayer-current-time";
+
+//     function addLocalStorage(){
+//         localStorage.setItem(KEY_LOCAL_STORAGE, videoplayerCurrentTime)
+//     }
+// });
+
+// player.setCurrentTime(30.456).then(function(seconds) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
