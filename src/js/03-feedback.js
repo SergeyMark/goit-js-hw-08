83b7c6d13bb1459ce02f5db7ea9de026234937ca
Sergey <110375004+SergeyMark@users.noreply.github.com>
2022-12-24 17:44:48 +0200

    add up

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,54 @@
+import throttle from 'lodash.throttle';
 
+const feedBackForm = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+
+const FORM_KEY = "feedback-form-state";
+
+outLocalStorage();
+
+// передать данные в обьекст и записать в локальное хранилище
+
+
+// message.addEventListener('input', messageInInput);
+
+
+
+feedBackForm.addEventListener('input', throttle(messageInInput, 500));
+
+function messageInInput(event) {
+
+    const emailEl = event.currentTarget.email.value;
+    const messageEl = event.currentTarget.message.value;
+
+    const formDataInput = {
+        emailEl,
+        messageEl
+    }
+
+    localStorage.setItem(FORM_KEY, JSON.stringify(formDataInput));
+}
+
+
+feedBackForm.addEventListener('click', onSubmitRemove);
+
+function onSubmitRemove(event) {
+    event.preventDefault();
+    
+    if (event.target.nodeName === 'BUTTON') {
+        event.currentTarget.reset();
+        localStorage.removeItem(FORM_KEY);   
+    }
+}
+
+function outLocalStorage() {
+    const saveOutPut = localStorage.getItem(FORM_KEY);
+
+    if (saveOutPut) {
+        const parsInput = JSON.parse(saveOutPut)
+        console.log(parsInput)
+        message.value = parsInput.messageEl;
+        email.value = parsInput.emailEl;
+    }
+}
